c008e5242c02b93314868071f112c2305f7306a7
VTMASHEGO500 <vascomashego@gmail.com>
2023-02-24 08:02:04 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,12 @@
 
 /**
  *_isupper - check if a letter is uppercase
- *@x: The number to be checked
+ *@c: The number to be checked
  * Return: 1 for uppercase or 0
  */
-int _isupper(int x)
+int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
 	return (1);
 	}
