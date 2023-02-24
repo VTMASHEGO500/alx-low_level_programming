184ec548064881ec840f199a18183aae744fe949
VTMASHEGO500 <vascomashego@gmail.com>
2023-02-24 14:51:28 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,14 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 10; c++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 1; i <= 14; j++)
 		{
 			if (j >= 10)
 				_putchar('1');
-			_putchar (j % 10 + '0')
+			_putchar (j % 10 + '0');
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
