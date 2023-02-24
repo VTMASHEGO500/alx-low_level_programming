e649db0681d9a11e81db119b72399772922b4763
VTMASHEGO500 <vascomashego@gmail.com>
2023-02-24 13:34:03 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multipy 2 int
+ *@a: first int
+ *@b: second int
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
