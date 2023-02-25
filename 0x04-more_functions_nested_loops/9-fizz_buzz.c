473e954c63f4e8fabd604327715da529621a5ca8
VTMASHEGO500 <vascomashego@gmail.com>
2023-02-24 16:42:11 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *main - print the number from 1 to 100
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n =1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 ! =0)
+		{
+			prinft(" fuzz")
+		} else if (n % 5 == 0 && i % 3 ! = 0)
+		{
+			printf(" buzz")
+		} else if (n % 3 == 0 && ! % 5 == 0)
+		{
+			printf(" fuzzbuzz");
+		} else if (n == 1)
+		{
+			printf("%d", n);
+		} else
+		{
+			printf(" %d", n);
+		}
+	}
+	printf("\n")
+
+	return: (0);
+
