dfa9ae057f83a6f0be51faeb252bdb1ac186d113
VTMASHEGO500 <vascomashego@gmail.com>
2023-02-24 16:00:39 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_line - draw a straight line
+ *@n: number of time char
+ */
+void print_line(void)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_square - print a square
+ *
+ *@size: size of the square
+ */
+void print_square(int size)
+{
+	if (n <= 0)
+	{
+		_putchar('\n')
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
