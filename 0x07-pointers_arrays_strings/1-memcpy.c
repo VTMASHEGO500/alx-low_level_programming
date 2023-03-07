74ab8ed7a2d4997b46b28d82184646e05c0d0925
VTMASHEGO500 <vascomashego@gmail.com>
2023-03-07 10:34:48 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,7 +4,7 @@
  * @dest: memory where is stored
  * @src: memory where is copied
  * @n: number of bytes
- * Return: dest
+ * Return: copied memory with n bytes changed
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
