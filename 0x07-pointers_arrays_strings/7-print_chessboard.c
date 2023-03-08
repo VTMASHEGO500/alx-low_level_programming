af3381ad716b1f2fcaefd81e48dfa52140ff6e9f
VTMASHEGO500 <vascomashego@gmail.com>
2023-03-07 22:49:34 -0800

    main.h

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- * print_chessboard -  function that prints the chessboard
+ * print_chessboard - function that prints the chessboard
  * @a: array
  * Return: Always 0 (Success)
  */
