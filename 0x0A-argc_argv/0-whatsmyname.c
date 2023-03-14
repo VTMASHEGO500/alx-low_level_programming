a5f1300d0b9c79979f781764337ec256b7f46d72
VTMASHEGO500 <vascomashego@gmail.com>
2023-03-14 13:42:05 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - Print the name of the program
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
+int atoi(const char *str);
+int check_num(char *str);
 
 #endif
