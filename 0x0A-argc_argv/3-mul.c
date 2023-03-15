3315c5e9a10f6ac73f3239841122a9e5e4fb4b4f
VTMASHEGO500 <vascomashego@gmail.com>
2023-03-15 01:08:41 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * _atoi - converts a string to an integer
@@ -7,6 +7,7 @@
  *
  * Return: the int converted from the string
  */
+
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
