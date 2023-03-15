4846733ba3dd1369aa4dfb62165f54302e6f33dc
VTMASHEGO500 <vascomashego@gmail.com>
2023-03-14 23:56:24 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,12 +10,11 @@
 
 int main(int argc, char *argv[])
 {
-	/*Declaring variables*/
-	int count = 0;
+	int i;
 
-	if (argc > 0)
+	for (i = 0; i < argc; i++)
 	{
-		/*WHILE - Print each arguments*/
-		while (count < argc)
-		{
-		printf("%s\n", argv[count]);
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
